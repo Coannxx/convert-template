3b39e6a72f6dd3aa54676c2915e4611907a1b71f
Coannxx <gd.vieira5555@gmail.com>
2025-08-27 04:20:40 -0300

    implement input validation for amount field in script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("javascript carregado")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+
+amount.addEventListener("input", function() {
+    const hasCaractersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCaractersRegex, "")
+})
+
